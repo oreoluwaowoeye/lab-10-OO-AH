2923a1a830346c2af3a6574d42fc0878204da0f2
oreoluwaowoeye <owoeyeo@ufl.edu>
2025-04-13 18:03:08 -0400

    modify calculator

--- calculator.py
+++ calculator.py
@@ -4,11 +4,27 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
 # First example
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
-    return b - a
+    return a - b
 
+def multiply(a, b):
+    return a * b
 
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Can not divide by 0.")
+    return b / a   # raise ZeroDivisionError if a == 0
+
+def logarithm(a, b):
+    try:
+        math.log(a,b)# use math library/raise ValueError
+    except ValueError as e:
+        print(f"ValueError: {e}")
+def exponent(a, b):
+    a**b
