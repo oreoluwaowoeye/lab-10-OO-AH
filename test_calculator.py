1250fcfe8a8ea2e33810d195007f6bfaddc0e7dc
oreoluwaowoeye <owoeyeo@ufl.edu>
2025-04-14 02:42:02 -0400

    hehehe

--- test_calculator.py
+++ test_calculator.py
@@ -46,7 +46,7 @@ class TestCalculator(unittest.TestCase):
     def test_log_invalid_argument(self): # 1 assertion
 
         with self.assertRaises(ValueError):
-            log(0,-2)
+            log(0,5)
         pass
 
     def test_hypotenuse(self): # 3 assertions
